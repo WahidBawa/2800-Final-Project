9adeda179a39f347c608a16bfe60844913c1f4b6
kevinliu8896 <kevinliu8896@gmail.com>
2021-03-15 15:59:41 -0400

    Updated

--- Drag_Racer/src/Road.java
+++ Drag_Racer/src/Road.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Road {
+import org.jogamp.java3d.*;
+import org.jogamp.java3d.utils.geometry.Sphere;
+import org.jogamp.java3d.utils.image.TextureLoader;
+import org.jogamp.java3d.utils.picking.PickResult;
+import org.jogamp.java3d.utils.picking.PickTool;
+import org.jogamp.java3d.utils.universe.SimpleUniverse;
+import org.jogamp.vecmath.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Road {
+// aleksa adds road here
+
+// Kevin adds lighting and fog
+    private static void createLight(BranchGroup bg) { // create light with ambient lighting with point light
+        AmbientLight ambientLight = new AmbientLight(true, new Color3f(0.2f, 0.2f, 0.2f)); // lightOn set to true, with Color3f(0.2f, 0.2f, 0.,2f)
+        ambientLight.setInfluencingBounds(new BoundingSphere(new Point3d(0,0,0), 100));// set influencing bounds with bouding sphere at point3d (0,0,0) with radius 100
+        bg.addChild(ambientLight); // add child to branch group with ambient light
+        PointLight pointLight = new PointLight(true, Commons.White, new Point3f(2, 2, 2), new Point3f(1, 0, 0));
+        pointLight.setInfluencingBounds(new BoundingSphere(new Point3d(0,0,0), 100));
+        bg.addChild(pointLight); // add point light to branch group
+    }
+
+    private static ExponentialFog createFog(Color3f clr, BoundingSphere bounds) {
+        ExponentialFog exponentialFog = new ExponentialFog(clr, 0.1f); // set up exponential fog with 0.1 density
+        exponentialFog.setInfluencingBounds(bounds);
+        return exponentialFog;
+    }
+
 }
